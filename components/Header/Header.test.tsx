7ae572b732139a28502dc0cc727b7c01952c2e7a
Your Name <cyberforge1@gmail.com>
2026-02-09 12:06:23 +1100

    updates tests

--- components/Header/Header.test.tsx
+++ components/Header/Header.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Header } from './Header';
 
 describe('Header', () => {
-  it('renders the title', () => {
-    render(<Header />);
-    expect(screen.getByText('Media Transform')).toBeInTheDocument();
+  it('renders the header', () => {
+    const { container } = render(<Header />);
+    expect(container.querySelector('.header')).toBeInTheDocument();
   });
 });
